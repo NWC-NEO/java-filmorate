8f8f3ccd5dac2c7c7a1337e3c12ce8397708f050
Артем Буренин <ironman1822@mail.ru>
2026-05-13 17:19:06 +0400

    fix: safe releaseDate mapping with null check

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,11 +13,13 @@ import java.sql.SQLException;
 public class FilmMapper implements RowMapper<Film> {
     @Override
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Date releaseDate = rs.getDate("release_date");
+
         return Film.builder()
                 .id(rs.getLong("id"))
                 .name(rs.getString("name"))
                 .description(rs.getString("description"))
-                .releaseDate(rs.getDate("release_date").toLocalDate())
+                .releaseDate(releaseDate != null ? releaseDate.toLocalDate() : null)
                 .duration(rs.getInt("duration"))
                 .mpa(new Mpa(rs.getInt("mpa_rating_id"), rs.getString("mpa_name")))
                 .build();
